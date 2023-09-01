d55260e039f793f49a6bc3dbde15cde474c27fe5
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-09-01 12:28:47 +0900

    16章終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,17 @@ class UsersController < ApplicationController
     @post_images = @user.post_images
   end
   def edit
-    
+    @user = User.find(params[:id])
   end
   def update
-    
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
   end
 end
